567bc77bfbd1b96818c646e797dbe4af9f310d98
teogomes <tptls@hotmail.com>
2018-11-08 10:57:48 +0200

    Initial Commit

---
+++ 9pixelsTests/_pixelsTests.swift
@@ -0,0 +1,34 @@
+//
+//  _pixelsTests.swift
+//  9pixelsTests
+//
+//  Created by Teodoro Gomes on 08/11/2018.
+//  Copyright © 2018 Teodoro Gomes. All rights reserved.
+//
+
+import XCTest
+@testable import _pixels
+
+class _pixelsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
